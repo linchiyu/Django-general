9fb594bb829f2da2948700655e3f1e222c7da53e
EnzoSalvadori <63012963+EnzoSalvadori@users.noreply.github.com>
2020-09-01 15:38:20 -0300

    Site funcionando
    site com cadastro de pessoas, lista de acessos, alteração de pessoas, alteração de senha e login

--- backend/controleacesso/models.py
+++ backend/controleacesso/models.py
@@ -1,23 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class PessoaManager(models.Manager):
+    def create_Pessoa(self,nome,codigo,foto,bloqueado):
+        pessoa = self.create(nome=nome,codigo=codigo,foto=foto,bloqueado=bloqueado)
+        return pessoa
+
 class Pessoa(models.Model):
     class Meta:
         db_table = 'pessoa'
-
     nome = models.CharField(max_length=40)
     codigo = models.CharField(max_length=20, blank=True, null=True)
-    foto = models.ImageField(upload_to='rosto')
+    foto = models.ImageField()
     bloqueado = models.BooleanField(default=False)
+
+    objects = PessoaManager()
 
     def __str__(self):
         return self.nome
 
-
 class Acesso(models.Model):
     class Meta:
         db_table = 'acesso'
-
     data = models.DateTimeField()
     tipoAcesso = models.CharField(max_length=10) #0 = entrada 1 = saida
     fkpessoa = models.ForeignKey(Pessoa, on_delete=models.PROTECT)
